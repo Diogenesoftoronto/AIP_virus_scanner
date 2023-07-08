2067f67c94f9bb6f6f84f657afcd5336ae669218
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-07-08 01:12:53 -0400

    Read a file from stdin and Start the Clamav engine
    The program will now scan files for virus using libclamav

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,37 @@ const std = @import("std");
 const clam = @import("c.zig");
 
 pub fn main() !void {
-    _ = clam.cl_init(0);
+    // const CL = enum { succes, failure, init_default, max_engine_scantime, db_stdopt };
+    // _ = CL;
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+
+    try stdout.print("Gimme a file you AIP", .{});
+
+    var read_buf: [80]u8 = undefined;
+
+    if (stdin.readUntilDelimiterOrEof(read_buf[0..], '\n')) |path| {
+        try stdout.print("Here is the file {?s}", .{path});
+        const true_path = path orelse "~/aip_path";
+        var fd: std.fs.File = try std.fs.cwd().openFile(true_path, .{
+            .mode = .read_only,
+        });
+        defer fd.close();
+    } else |err| switch (err) {
+        error.StreamTooLong => std.debug.print("Your file name was too long, try a shorter file.", .{}),
+        else => std.debug.print("The were other unexpected errors {}", .{err}),
+        // error.EndOfStream => std.debug.print("The stream terminated unexpectedly."),
+    }
+
+    var ret = clam.cl_init(clam.CL_INIT_DEFAULT);
+    if (ret != clam.CL_SUCCESS) {
+        std.debug.print("Failed to initialize clamav.", .{});
+        std.os.exit(1);
+    }
+
     var engine = clam.cl_engine_new();
-    _ = clam.cl_engine_free(engine);
+    if (engine == null) {
+        std.debug.print("The engine could not begin.", .{});
+        std.os.exit(1);
+    }
 }
